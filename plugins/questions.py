13f084a9af724b24eed74a224c7577006314607e
nylan <nylan@nylan.cat>
2025-10-02 16:25:41 -0400

    fixing questions

--- plugins/questions.py
+++ plugins/questions.py
@@ -13,25 +13,73 @@ class QuestionPlugin:
         self.name = "questions"
         self.priority = 35
         self.enabled = True
-        
+
         self.yes_no_responses = [
             "Yes!", "No!", "Maybe!", "Definitely!", "Absolutely not!",
             "I think so!", "Probably not...", "Ask again later!",
             "Signs point to yes!", "Don't count on it!", "Most likely!",
             "Cannot predict now!", "Yes, definitely!", "Very doubtful!"
         ]
-        
+
         self.what_responses = [
             "That's a mystery!", "Beats me!", "Good question!",
             "I have no idea!", "Your guess is as good as mine!",
             "Something interesting, I'm sure!", "Nobody knows!",
             "The answer is 42!", "That would be telling!"
         ]
-        
+
         self.who_responses = [
             "Someone special!", "A person!", "Nobody important!",
             "Your mom!", "The person behind you!", "A mystery person!",
             "Someone you know!", "Not me!", "The usual suspect!"
+        ]
+
+        # How many responses
+        self.how_many_responses = [
+            "3! No, wait! 4. NO! I lost 2... so 2. 1?",
+            "Seven! Definitely seven. Or was it eight?",
+            "All of them! *counts on fingers* Wait, none of them!",
+            "42! No! 17! Actually... *shrugs* maybe 5?",
+            "More than you! Less than... wait, what was the question?",
+            "Eleventy-seven! Is that a number?",
+            "Let me count... 1, 2, skip a few... 99, 100!",
+            "Not enough! Or too many? *panics* I don't know!"
+        ]
+
+        # Favorite/which responses
+        self.favorite_responses = [
+            "Cat! No! French fries! No! Can I ask my master?",
+            "The purple one! Wait, the shiny one! *spins*",
+            "Uh... *sweats* ...yes?",
+            "All of them! None of them! The one in the middle!",
+            "That's like asking me to pick a favorite electron!",
+            "The one that goes 'beep'! Or was it 'boop'?",
+            "I plead the fifth! Wait, what's a fifth?",
+            "Ooh! Ooh! The... uh... *forgets* ...what were we talking about?"
+        ]
+
+        # When/where responses
+        self.when_where_responses = [
+            "Tuesday! No, wait! Next week! Or was it last year?",
+            "Over there! *points vaguely in all directions*",
+            "At precisely... *checks nonexistent watch* ...time o'clock!",
+            "In the place! You know, THE place!",
+            "Soon! Or later! Definitely one of those!",
+            "Behind you! No, in front! To the left! *spins*",
+            "When the moon is full and the chickens dance!",
+            "Right here! No, there! Everywhere? Nowhere?"
+        ]
+
+        # Why/how responses
+        self.why_how_responses = [
+            "Because reasons! Very important reasons!",
+            "With my hands! Or feet! Or... wings? Do I have wings?",
+            "Very carefully! Or not at all! One of those!",
+            "The universe works in mysterious ways!",
+            "Magic! It's always magic! Or science! One of those!",
+            "By pressing the buttons! All the buttons!",
+            "Because someone told me to! Or didn't! I forget!",
+            "Through the power of friendship! And explosions!"
         ]
     
     async def handle_message(self, bot, nick: str, channel: str, message: str) -> bool:
@@ -46,13 +94,27 @@ class QuestionPlugin:
         if not bot_mentioned and random.random() > 0.1:
             return False
 
-        # Choose response based on question type
-        if re.search(r'(?i)\b(is|are|will|can|could|should|would|do|does|did)\b', message):
-            response = random.choice(self.yes_no_responses)
-        elif re.search(r'(?i)\bwhat\b', message):
-            response = random.choice(self.what_responses)
+        # Choose response based on question type (order matters - check specific types first)
+        if re.search(r'(?i)\bhow many\b', message):
+            response = random.choice(self.how_many_responses)
+        elif re.search(r'(?i)\bhow much\b', message):
+            response = random.choice(self.how_many_responses)
+        elif re.search(r'(?i)\b(favorite|favourite|which)\b', message):
+            response = random.choice(self.favorite_responses)
+        elif re.search(r'(?i)\bwhen\b', message):
+            response = random.choice(self.when_where_responses)
+        elif re.search(r'(?i)\bwhere\b', message):
+            response = random.choice(self.when_where_responses)
+        elif re.search(r'(?i)\bwhy\b', message):
+            response = random.choice(self.why_how_responses)
+        elif re.search(r'(?i)\bhow\b', message):
+            response = random.choice(self.why_how_responses)
         elif re.search(r'(?i)\bwho\b', message):
             response = random.choice(self.who_responses)
+        elif re.search(r'(?i)\bwhat\b', message):
+            response = random.choice(self.what_responses)
+        elif re.search(r'(?i)\b(is|are|will|can|could|should|would|do|does|did)\b', message):
+            response = random.choice(self.yes_no_responses)
         else:
             response = random.choice(self.yes_no_responses)
 
